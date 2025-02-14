8b0a0334991402bfce8062aa900ca59faaa55f92
Barthur B <alexbergeron15@gmail.com>
2025-02-14 14:18:44 -0500

    exercise 3.3

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -46,6 +46,18 @@ app.get("/api/persons", (request, response) => {
   console.log("persons =", persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    response.json(person);
+    console.log(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
