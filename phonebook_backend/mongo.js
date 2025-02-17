86c7084040935d57b4b0a4c91005c3ad21736d19
Barthur B <alexbergeron15@gmail.com>
2025-02-17 10:42:09 -0500

    exercise 3.12

---
+++ phonebook_backend/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = encodeURIComponent(process.argv[2]);
+const personName = process.argv[3];
+const personNumber = process.argv[4];
+
+const url = `mongodb+srv://alexbergeron15:${password}@cluster0.mj8jx.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (!personName) {
+  console.log("phonebook:");
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
+  });
+} else {
+  const person = new Person({
+    name: personName,
+    number: personNumber,
+  });
+
+  person.save().then((result) => {
+    console.log(`added ${personName} number ${personNumber} to phonebook`);
+    mongoose.connection.close();
+  });
+}
